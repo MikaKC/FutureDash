dab752abcf887aeeb0d294b69ed3363f49ab6c41
MikaKC <62851960+MikaKC@users.noreply.github.com>
2022-11-27 17:20:58 -0500

    std::to_string

--- src/Callbacks.cpp
+++ src/Callbacks.cpp
@@ -8,13 +8,13 @@ void Callbacks::onInfoButtonPressed(cocos2d::CCObject* pSender)
 void Callbacks::onMoreInfoPressed(cocos2d::CCObject* pSender)
 {
     // You can come and kill me iyw
-    std::string playerCube = "Cube ID: " + std::_Integral_to_string<char>(gd::GameManager::sharedState()->getPlayerFrame());
-    std::string playerShip = "\nShip ID: " + std::_Integral_to_string<char>(gd::GameManager::sharedState()->getPlayerShip());
-    std::string playerBall = "\nBall ID: " + std::_Integral_to_string<char>(gd::GameManager::sharedState()->getPlayerBall());
-    std::string playerUFO = "\nUFO ID: " + std::_Integral_to_string<char>(gd::GameManager::sharedState()->getPlayerBird());
-    std::string playerWave = "\nWave ID: " + std::_Integral_to_string<char>(gd::GameManager::sharedState()->getPlayerDart());
-    std::string playerRobot = "\nRobot ID: " + std::_Integral_to_string<char>(gd::GameManager::sharedState()->getPlayerRobot());
-    std::string playerSpider = "\nSpider ID: " + std::_Integral_to_string<char>(gd::GameManager::sharedState()->getPlayerSpider());
+    std::string playerCube = "Cube ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerFrame());
+    std::string playerShip = "\nShip ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerShip());
+    std::string playerBall = "\nBall ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerBall());
+    std::string playerUFO = "\nUFO ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerBird());
+    std::string playerWave = "\nWave ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerDart());
+    std::string playerRobot = "\nRobot ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerRobot());
+    std::string playerSpider = "\nSpider ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerSpider());
 
     ALERT("Icon info", "Ok", nullptr, std::string(playerCube + playerShip + playerBall + playerUFO + playerWave + playerRobot + playerSpider));
 }
