174fb7f5f68b2270b8977f133088e726a35b0df3
MikaKC <62851960+MikaKC@users.noreply.github.com>
2022-11-27 18:27:27 -0500

    Add files via upload

--- src/Callbacks.cpp
+++ src/Callbacks.cpp
@@ -8,13 +8,13 @@ void Callbacks::onInfoButtonPressed(cocos2d::CCObject* pSender)
 void Callbacks::onMoreInfoPressed(cocos2d::CCObject* pSender)
 {
     // You can come and kill me iyw
-    std::string playerCube = "Cube ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerFrame());
-    std::string playerShip = "\nShip ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerShip());
-    std::string playerBall = "\nBall ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerBall());
-    std::string playerUFO = "\nUFO ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerBird());
-    std::string playerWave = "\nWave ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerDart());
-    std::string playerRobot = "\nRobot ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerRobot());
-    std::string playerSpider = "\nSpider ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerSpider());
+    std::string playerCube = "<cr>Cube</c> ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerFrame());
+    std::string playerShip = "\n<co>Ship</c> ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerShip());
+    std::string playerBall = "\n<cy>Ball</c> ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerBall());
+    std::string playerUFO = "\n<cg>UFO</c> ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerBird());
+    std::string playerWave = "\n<cb>Wave</c> ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerDart());
+    std::string playerRobot = "\n<cp>Robot</c> ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerRobot());
+    std::string playerSpider = "\n<cr>Spider</c> ID: " + std::to_string(gd::GameManager::sharedState()->getPlayerSpider());
 
     ALERT("Icon info", "Ok", nullptr, std::string(playerCube + playerShip + playerBall + playerUFO + playerWave + playerRobot + playerSpider));
 }
